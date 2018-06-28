dbe120c1c5215a4448276215023704de22185ed7
wangqianwen0418 <wangqianwen0418@163.com>
2018-06-28 12:22:24 +0800

    add: boxplot

--- src/components/FeatureChart.tsx
+++ src/components/FeatureChart.tsx
@@ -1,61 +1,108 @@
 import ReactEcharts from "echarts-for-react";
 import * as React from "react";
+import {prepareBoxplotData} from '../helper';
 
 import {IFeature} from './DataView';
 
 export default class FeatureChart extends React.Component<{feature:IFeature, classes:IFeature, cate_classes: number[]}>{
     getOption(){
         let {feature, classes, cate_classes} = this.props
+        
+        // const title = cate_classes.map((d, idx)=>{
+        //     return {
+        //         text: `class_${d}`,
+        //         textBaseline: 'middle',
+        //         textStyle:{
+        //             fontSize: 12
+        //         },
+        //         top: (idx +  0.2 ) * 100 / (cate_classes.length) + '%',
+        //     } 
+        // })
 
-        const title = cate_classes.map((d, idx)=>{
-            return {
-                text: `class_${d}`,
-                textBaseline: 'middle',
-                textStyle:{
-                    fontSize: 12
-                },
-                top: (idx +  0.2 ) * 100 / (cate_classes.length) + '%',
-            } 
-        })
-        const singleAxis = cate_classes.map((d, idx)=>{
-            return {
-                left: '20%',
-                boundaryGap: false,
-                height: '5%',
-                top: (idx + 0.2 ) * 100 / (cate_classes.length) + '%',
-                axisLabel: {
-                    interval: 2
-                }
-            }
-        })
+        // const singleAxis = cate_classes.map((d, idx)=>{
+        //     return {
+        //         left: '20%',
+        //         boundaryGap: false,
+        //         height: '5%',
+        //         top: (idx + 0.2 ) * 100 / (cate_classes.length) + '%',
+        //         axisLabel: {
+        //             interval: 2
+        //         }
+        //     }
+        // })
+        
         const series = cate_classes.map((d, f_idx)=>{
             let data:any[] = []
             feature.data.forEach((d, idx)=>{
                 if(classes.data[idx]==f_idx){
-                    data.push(d)
+                    data.push([d, f_idx])
                 }
             })
             return {
-                singleAxisIndex: f_idx,
-                coordinateSystem: 'singleAxis',
                 type: 'scatter',
+                symbolSize: 6,
+                itemStyle: {
+                    opacity: 0.3
+                },
                 data,
             }
         })
+        
+        let boxData = prepareBoxplotData(
+            series.map(d=>{
+                //from Array<[feature_value, classes_value]> to Array<feature_value>
+                return d.data.map(d=>d[0])
+            }), 
+            {layout: 'vertical'}
+        );
+        let boxSeries = {
+                type: 'boxplot',
+                itemStyle: {
+                    borderWidth: 1,
+                    borderColor: '#444',
+                },
+                data: boxData.boxData
+            }
+
+        // series.concat(boxSeries)
+
         const option = {
             tooltip: {
                 position: 'top'
             },
-            title,
-            singleAxis,
-            series,
+            legend:{},
+            grid:{
+                left: '20%',
+                height: '60%',
+                top:'10%',
+            },
+            title: { text: feature.name},
+            // singleAxis,
+            xAxis: {
+                type:'value',
+                min: 'dataMin',
+                max: 'dataMax',
+            },
+            yAxis: {
+                type:"category",
+                data: cate_classes.map(d=>'class_'+d),
+                axisLine: {
+                    show: false
+                },
+                axisLabel:{
+                    show: false
+                },
+                axisTick:{
+                    show: false
+                },
+            },
+            series:[boxSeries, ...series],
         };
         return option
     }
     render(){
-        const {feature} = this.props
-        return <div className="feature" style={{height: '20%'}}>
-            <div className='featureTitle'>{feature.name}</div>
+        return <div className="feature" style={{height: '10%'}}>
+            {/* <div className='featureTitle'>{feature.name}</div> */}
             <ReactEcharts 
             option = { this.getOption() }
             style={{height: `100%`, width: '100%'}}
--- src/helper/index.tsx
+++ src/helper/index.tsx
@@ -3,14 +3,14 @@ const COLORS: string[] = [
     // ' #9e0142',
     ' #fdae61',
     // ' #d53e4f',
-    // ' #e6f598',
+    ' #a0ef98',
     ' #f46d43',
     // ' #ffffbf',
     ' #66c2a5',
     ' #3288bd',
     ' #5e4fa2',
     ' #d53e4f',
-    // ' #fee08b',
+    ' #fee08b',
     ]
 
 const GREEN: string[] = [
@@ -93,4 +93,65 @@ const csv2json=(csv: string|any)=>{
     return keys
 }
 
-export { getColor, csv2json }
+function asc(arr:number[]) {
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+    return arr;
+}
+
+var quantile = function(ascArr:number[], p:number) {
+    var H = (ascArr.length - 1) * p + 1,
+        h = Math.floor(H),
+        v = +ascArr[h - 1],
+        e = H - h;
+    return e ? v + e * (ascArr[h] - v) : v;
+};
+
+
+var prepareBoxplotData = function (rawData:any[], opt:any) {
+    opt = opt || [];
+    var boxData = [];
+    var outliers = [];
+    var axisData = [];
+    var boundIQR = opt.boundIQR;
+    var useExtreme = boundIQR === 'none' || boundIQR === 0;
+
+    for (var i = 0; i < rawData.length; i++) {
+        axisData.push(i + '');
+        var ascList = asc(rawData[i].slice());
+
+        var Q1 = quantile(ascList, 0.25);
+        var Q2 = quantile(ascList, 0.5);
+        var Q3 = quantile(ascList, 0.75);
+        var min = ascList[0];
+        var max = ascList[ascList.length - 1];
+
+        var bound = (boundIQR == null ? 1.5 : boundIQR) * (Q3 - Q1);
+
+        var low = useExtreme
+            ? min
+            : Math.max(min, Q1 - bound);
+        var high = useExtreme
+            ? max
+            : Math.min(max, Q3 + bound);
+
+        boxData.push([low, Q1, Q2, Q3, high]);
+
+        for (var j = 0; j < ascList.length; j++) {
+            var dataItem = ascList[j];
+            if (dataItem < low || dataItem > high) {
+                var outlier = [i, dataItem];
+                opt.layout === 'vertical' && outlier.reverse();
+                outliers.push(outlier);
+            }
+        }
+    }
+    return {
+        boxData: boxData,
+        outliers: outliers,
+        axisData: axisData
+    };
+};
+
+export { getColor, csv2json, prepareBoxplotData }
