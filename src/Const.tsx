4bae3bad1f1232dfe3b590d081fb9caca846f1ef
Ming Yao <yaoming.thu@gmail.com>
2018-07-02 20:31:32 +0800

    fix enter_data error

--- src/Const.tsx
+++ src/Const.tsx
@@ -1,2 +1,4 @@
-export const URL = 'http://atm.hkustvis.org' 
+export const URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : location.origin;
 // export const URL = '0.0.0.0:3333'
