6ad3be648c5da20c4fc42dce51b19e2b6da4d05c
jnzhihuoo1 <593757535@qq.com>
2018-09-10 12:27:26 +0800

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -96,6 +96,7 @@ class App extends React.Component<{}, IState> {
                 name:this.user_name,
                 datasetID:this.state.datasetID?this.state.datasetID:-1,
                 datarunID:this.state.datarunID?this.state.datarunID:-1,
+                version:"BASELINE",
                 clickevent:log
             }
             postBundleClickEvent(bundlelog);
