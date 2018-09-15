365b843e7b33fadf4001bc8b7bcec72cf1eebeaa
jnzhihuoo1 <593757535@qq.com>
2018-09-15 16:39:26 +0800

    update

--- src/Const.tsx
+++ src/Const.tsx
@@ -3,7 +3,7 @@ export const URL = process.env.NODE_ENV === 'development'
     : location.origin;
 
 export const UPDATE_INTERVAL_MS = 5000;
-export const USER_STUDY = false;
+export const USER_STUDY = true;
 // user_study == false , then user study related modules will be disabled.
 // including, set name, post click event, early stoppings.
 
